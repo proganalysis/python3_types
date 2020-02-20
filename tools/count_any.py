32442661a29a185a4f06c95a53466c798c2439f9
Daniel McCrevan <danielmccrevan@gmail.com>
2020-02-19 19:47:44 -0500

    Added better stats

--- tools/count_any.py
+++ tools/count_any.py
@@ -1,50 +1,69 @@
 import re, os, sys
 
+func_stats = {"any": 0, "bool": 0, "str": 0, "int": 0}
+assign_stats = {"any": 0, "bool": 0, "str": 0, "int": 0}
 func_total = 0
-func_any_total = 0
-reg_func = "-> .+:"
-
 assign_total = 0
-assign_any_total = 0
+reg_func = "-> .+:"
 reg_assign = ": .+"
 
-def count_any(f):
+def count(s, assign=False, func=False):
     global func_total
-    global func_any_total
     global assign_total
-    global assign_any_total
+    global func_stats
+    global assign_stats
+
+    if assign:
+        if 'Any' in s:
+            assign_stats['any'] += 1
+        elif 'bool' in s:
+            assign_stats['bool'] += 1
+        elif 'str' in s:
+            assign_stats['str'] += 1
+        elif 'int' in s:
+            assign_stats['int'] += 1
+        assign_total += 1
+    elif func:
+        if 'Any' in s:
+            func_stats['any'] += 1
+        elif 'bool' in s:
+            func_stats['bool'] += 1
+        elif 'str' in s:
+            func_stats['str'] += 1
+        elif 'int' in s:
+            func_stats['int'] += 1
+        func_total += 1
+
+
+def analyze_files(f):
     fd = open(f, "r")
     lines = fd.readlines()
     for line in lines:
         # Check for func
         res = re.findall(reg_func, line)
         if len(res) == 1:
-            if 'Any' in res[0]:
-                func_any_total += 1
-            func_total += 1
+            count(res[0], func=True)
         # Check for assign
         res = re.findall(reg_assign, line)
         if len(res) > 0:
             for x in res:
-                if 'Any' in x:
-                    assign_any_total += 1
-                assign_total += 1
-
-
+                count(x, assign=True)
 
 def main():
-    d = "../allrepo/stripped/" 
+    d = "../big/allrepo/stripped/" 
     for root, dirs, files in os.walk(d):
         for file in files:
             if file.endswith(".pyi"):
-                count_any(os.path.join(root, file))
+                analyze_files(os.path.join(root, file))
     print(f"Total functions: {func_total}")
-    print(f"Any return types: {func_any_total}")
-    print(f"Percent: {func_any_total/func_total}")
+    for x in func_stats.keys():
+        print(f"Total functions with {x} return type: {func_stats[x]}") 
+        print(f"Percent of functions with this return type: {(func_stats[x]/func_total) * 100}")
     print()
     print(f"Total assignments: {assign_total}")
-    print(f"Any-typed assignments: {assign_any_total}")
-    print(f"Percent: {assign_any_total/assign_total}")
+    for x in assign_stats.keys():
+        print(f"Total assignments with {x} type: {assign_stats[x]}") 
+        print(f"Percent of assignments with this type: {(assign_stats[x]/assign_total) * 100}")
 
 if __name__ == "__main__":
     main()
